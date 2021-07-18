429e66716143f557a806352bdd100b78032f51fd
Rodolpho Alves <rodolpho.alves@zup.com.br>
2021-07-18 19:52:09 -0300

    outros usos jpql

--- src/main/java/com/alura/test/JpqlMovimentacaoCategoria.java
+++ src/main/java/com/alura/test/JpqlMovimentacaoCategoria.java
@@ -1,12 +1,16 @@
 package com.alura.test;
 
+import com.alura.model.Categoria;
 import com.alura.model.Conta;
 import com.alura.model.Movimentacao;
 
-import javax.persistence.*;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
 import java.util.List;
 
-public class Jpql {
+public class JpqlMovimentacaoCategoria {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("alura");
         EntityManager em = emf.createEntityManager();
@@ -14,15 +18,20 @@ public class Jpql {
         Conta conta = new Conta();
         conta.setId(2L);
 
-        String jpql = "select m from Movimentacao m where m.conta = :pConta order by m.valor desc";
+        String jpql = "select m from Movimentacao m join m.categorias c" +
+                "where c = :pCategoria";
+
+        Categoria categoria = new Categoria();
+        categoria.setId(2L);
 
         TypedQuery<Movimentacao> query = em.createQuery(jpql, Movimentacao.class);
-        query.setParameter("pConta", conta);
+        query.setParameter("pCategoria", categoria);
 
         List<Movimentacao> resultList = query.getResultList();
 
         resultList.forEach(
                 movimentacao -> {
+                    System.out.println(movimentacao.getCategorias().size());
                     System.out.println(movimentacao.getDescricao());
                     System.out.println(movimentacao.getTipoMovimentacao());
                     System.out.println(movimentacao.getValor());
